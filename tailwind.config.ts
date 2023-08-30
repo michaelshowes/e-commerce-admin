12736c6f02647094ce7b65f7fd6901b254085e4c
Michael Showes <michael.showes@gmail.com>
2023-08-29 21:37:19 -0400

    configured global styles and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,46 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		screens: {
+			tablet: '768px',
+			desktop: '1200px'
+		},
+		colors: {
+			white: '#ffffff',
+			black: '#0000112',
+			'gray-100': '#3e3f4e',
+			'gray-200': '#2b2c37',
+			'gray-300': '#20212c',
+			'slate-100': '#f4f7fd',
+			'slate-200': '#e4ebfa',
+			'slate-300': '#828fa3',
+			'violet-100': '#a8a4ff',
+			'violet-200': '#635fc7',
+			'red-100': '#ff9898',
+			'red-200': '#ea5555'
+		},
+		fontSize: {
+			xs: ['0.75rem', { lineHeight: '0.9375rem', fontWeight: 'bold' }],
+			sm: ['0.8125rem', { lineHeight: '1.4375rem' }],
+			'heading-s': [
+				'0.75rem',
+				{ lineHeight: '0.9375rem', letterSpacing: '0.15em', fontWeight: 'bold' }
+			],
+			'heading-m': ['0.9375rem', { lineHeight: '1.25rem', fontWeight: 'bold' }],
+			'heading-l': [
+				'1.125rem',
+				{ lineHeight: '1.4375rem', fontWeight: 'bold' }
+			],
+			'heading-xl': ['1.5rem', { lineHeight: '1.875rem', fontWeight: 'bold' }]
+		},
+		extend: {}
+	},
+	plugins: []
+};
+export default config;
